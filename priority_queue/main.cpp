3dd0aa779da1c40d2a36d66de983f1e06f32a83c
Gustav R. Jansen <gustav.r.jansen@gmail.com>
2020-11-05 09:56:39 -0500

    Added multithreaded version of priority_queue.

--- priority_queue/main.cpp
+++ priority_queue/main.cpp
@@ -1,7 +1,12 @@
 #include <cstddef>
 #include <iostream>
 #include <queue>
+#include <thread>
+#include <mutex>
 #include "message.hpp"
+
+int data[] { 10, 20, 30 };
+std::vector<Message*> messages[10];
 
 void print_queue(std::priority_queue<Message>& q) {
   while(!q.empty()) {
@@ -19,10 +24,23 @@ void print_queue_of_ptr(std::priority_queue<Message*, std::vector<Message*>, Cmp
   }
 }
 
+std::mutex mtx;
+std::priority_queue<Message*, std::vector<Message*>, CmpMsgTimestamp> q3;
+void add_to_queue(int i) {
+  std::cout << "I am thread #" << i << std::endl;
+  int indices[] {4, 7, 1, 5, 2, 9, 3, 8, 6, 0};
+  for (int idx : indices) {
+    messages[i].push_back(new Message((std::byte*) data, 10*(9-i)+idx));
+  }
+  for (Message* m: messages[i]) {
+    mtx.lock();
+    q3.push(m);
+    mtx.unlock();
+  }
+}
 int main() {
 
   std::cout << "Priority queue: std::priority_queue<Message>\n";
-  int data[] { 10, 20, 30 };
   int indices[] {4, 7, 1, 5, 2, 9, 34, 22, 57, 6, 12, 45};
 
   std::priority_queue<Message> q;
@@ -45,5 +63,13 @@ int main() {
   }
   print_queue_of_ptr(q2);
 
+  std::thread threads[10];
+  for ( int i=0; i<10;i++ )
+    threads[i] = std::thread(add_to_queue, i);
+
+  for (auto& th : threads) th.join();
+
+  std::cout << "Priority queue: std::priority_queue<Message*> - threaded\n";
+  print_queue_of_ptr(q3);
   return 0;
 }
